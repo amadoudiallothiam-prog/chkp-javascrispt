7db68eb7368a56e2b22c6cac36c19a2b9b0ce964
amadou <amadoudiallothiam@gmail.com>
2026-06-10 20:26:19 +0000

    javascript

---
+++ exo.js
@@ -0,0 +1,75 @@
+//exercices 1
+
+function somme(a,b) {
+    return a + b 
+}
+----------------------------------------
+//exercices 2
+
+function produit(a,b) {
+    return a * b
+}
+
+-----------------------------------------
+
+//exercices 3
+
+function verifier age(age) {
+    if (age >= 18)
+        console.log("majeur")
+    else
+        console.log("mineur")
+}
+------------------------------------------
+
+//exercices 4
+
+function verifierNombre(nombre) {
+    if (nombre > 0) {
+        console.log("Positif");
+    } else if (nombre < 0) {
+        console.log("Négatif");
+    } else {
+        console.log("Zéro");
+    }
+}
+---------------------------------------------
+// Exercices 5
+function jourDeLaSemaine(num) {
+    switch (num) {
+        case 1:
+            return "Lundi";
+        case 2:
+            return "Mardi";
+        case 3:
+            return "Mercredi";
+        case 4:
+            return "Jeudi";
+        case 5:
+            return "Vendredi";
+        case 6:
+            return "Samedi";
+        case 7:
+            return "Dimanche";
+        default:
+            return "Jour invalide";
+    }
+}
+----------------------------------------------
+Exercices 6
+function appreciation(note) {
+    switch (note) {
+        case "A":
+            return "Excellent";
+        case "B":
+            return "Bien";
+        case "C":
+            return "Passable";
+        case "D":
+            return "Insuffisant";
+        default:
+            return "Note invalide";
+    }
+}
+------------------------------------------------
+exercices 7
